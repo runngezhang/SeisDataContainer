48c9765653bb9d060ed58cef0b4d1fcb217fc8a4
klensink <keeganlensink@gmail.com>
2016-07-27 14:24:47 -0700

    SampleRange and HeaderBytes are now optional inputs when creating a data container using segyCon. Added the necessary documentation to the segyCon description

--- @segyCon/segyCon.m
+++ @segyCon/segyCon.m
@@ -4,31 +4,51 @@ classdef segyCon < iroCon
     %   files. Current implementation only reads data and does not
     %   overwrite the underlying SEG-Y data.
     %
-    %   container = segyCon(metadata_path, type, samples_range, header_bytes) returns
-    %   a segy seismic data container that accesses the data referenced by a given 
-    %   metadata file.
-    %--------------------------------------------------------------------------------------
-    %	Inputs: metadata_path: A string path to the metadata file.
-    %			Ex: 'BPshotsMeta.mat'
-    %		
-    %		type: Type of ensemble. Use either 'shot' for data, or 'stack' for models.
-    %		      Changes what kind of labels are used for the volume.
-    %	
-    %		samples_range: Range of samples you want to be read from each trace.
-    %			       Inputting an empty vector ( [] ) will read all samples.
-    %			       Ex: SampleRange = [50 500]
+    %	Use: 
+    %   	container = segyCon(metadata_path, type) 
     %
-    %		header_bytes: A vector containing the starting byte of trace attributes in
-    %			     the STHs that will store as metadata
-    %			     Ex: Header_Bytes=[1   81   85]; 
-    %		             This will store the Trace Number, Group X and Group Y
+    %	Which returns a segy seismic data container that accesses the data referenced 
+    %   by a given metadata file.
+    %--------------------------------------------------------------------------------------
+    %	Inputs: 
+    %
+    %           *MANDATORY* Metadata_path:  A string path to the metadata file. 
+    %			         Ex: 'BPshotsMeta.mat'
+    %		
+    %		*MANDATORY* Type: Type of ensemble. Use either 'shot' for data, or
+    %		                  'stack' for models. Changes what kind of labels
+    %                             are used for the volume.
+    %	
+    %		SampleRange: Optional range of samples you want to be read from each trace.
+    %			       
+    %		   Ex: Con=segyCon('meta','shot','SampleRange',[50 500])
+    %
+    %		Header_Bytes: An optional vector containing the starting byte of 
+    %			      trace attributes in the STHs that will store as metadata.
+    %			      If no header bytes are inputted by the user, the header bytes 
+    %			      chosen when scanning the file will be used.
+    %
+    %		   Ex: Con=segyCon('meta,'shot','Header_Bytes',[1 81 85]; 
+    %			     This will store the Trace Number, Group X and Group Y
     %			     positions.
     %
-    %	Output: Container: A seismic data container that will access data according 
-    %                      to the given metadata file.
-    %------------------------------------------------------------------------------------- 
+    %	Output: 
+    %	   Container: A seismic data container that will access data/header values
+    %                 corresponding to the given metadata file.
+    %-------------------------------------------------------------------------------------
     %	Container Methods
     %		
+    %		Con.blocks(blocks)
+    %			Will load the header values chosen when creating
+    %	                the container, as well as the data, for every trace in the blocks
+    %		        called.
+    %
+    %		Con.headers(blocks)
+    %			Will load only the header values chosen when creating
+    %	                the container for every trace in the blocks called. A good way to
+    % 			make acquisition masks for large data sets. 
+    %
+    %-------------------------------------------------------------------------------------
 
 
 
@@ -36,7 +56,7 @@ classdef segyCon < iroCon
     
     methods
         
-        function obj = segyCon(metadata_path, type, samples_range, header_bytes) 
+        function obj = segyCon(metadata_path, type, varargin) 
             
             switch type
                
@@ -54,19 +74,48 @@ classdef segyCon < iroCon
             obj = obj@iroCon(header, dims);
             obj.pathname = metadata_path;
             obj.type = type;
+
+% ---------------------------------------------------         
+            %Parse inputs for validity
+            narginchk(2,6)
             
-            if isempty(samples_range);
-            	obj.samples_range=[];
-            else
-            	obj.samples_range=samples_range;
-            end
             
-            if isempty(header_bytes);
-            	obj.header_bytes=[];
-            else
-            	obj.header_bytes=header_bytes;
-            end
-            
+		
+		% Check and account for optional input arguments
+		sr_written=0; hb_written=0;
+		
+		if isempty(varargin)
+			obj.samples_range = [];
+			obj.header_bytes = [];
+		elseif mod(length(varargin),2) == 1
+			error(['Odd number of optional input parameters. Make'...
+			     ' sure to use keywords'])
+		else
+			for i = 1:2:length(varargin)
+				if ischar(varargin{i});
+					if strcmpi(varargin{i},'SampleRange');
+						obj.samples_range = varargin{i+1};
+						sr_written=1;
+					elseif strcmpi(varargin{i},'Header_Bytes');
+						obj.header_bytes = varargin{i+1};
+						hb_written=1;
+					end
+				else 
+					error(['Invalid input format. Use Keywords'...
+					      'followed by their values'])
+				end
+
+	
+				if sr_written == 0;
+					obj.samples_range = [];
+				elseif hb_written == 0;
+					obj.header_bytes = [];
+				end
+		
+			end
+		end
+	
+   %-----------------------------------------------------------------------------        
             
         end
         
